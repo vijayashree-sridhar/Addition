7a31d26f69e5aa9eff0a3bbe001ae85e2dfb7bf6
vijayashree-sridhar <113469666+vijayashree-sridhar@users.noreply.github.com>
2023-05-01 23:56:18 +0530

    Update test_calculate_sum.py

--- test_calculate_sum.py
+++ test_calculate_sum.py
@@ -1,5 +1,15 @@
 from calculate_sum import calculate_sum
-import sys
 
-def test_calculate_sum():
-    assert calculate_sum(sys.argv[0], sys.argv[1]) == sys.argv[2]
+def pytest_addoption(parser):
+    parser.addoption("--a", action="store", type=int, help="first number")
+    parser.addoption("--b", action="store", type=int, help="second number")
+
+def test_calculate_sum(request):
+    a = request.config.getoption("--a")
+    b = request.config.getoption("--b")
+    result = calculate_sum(a, b)
+    assert result == a + b
+
+
+# def test_calculate_sum():
+#     assert calculate_sum(2, 3) == 5
