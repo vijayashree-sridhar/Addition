4b0e9718bc1ac5d719132337d4f37ea6c7c0c78a
vijayashree-sridhar <113469666+vijayashree-sridhar@users.noreply.github.com>
2023-05-02 00:06:51 +0530

    Update test_calculate_sum.py

--- test_calculate_sum.py
+++ test_calculate_sum.py
@@ -1,4 +1,4 @@
-# from calculate_sum import calculate_sum
+from calculate_sum import calculate_sum
 
 # def pytest_addoption(parser):
 #     parser.addoption("--a", action="store", type=int, help="first number")
@@ -11,27 +11,27 @@
 #     assert result == a + b
 
 
-# def test_calculate_sum():
-#     assert calculate_sum(2, 3) == 5
-
-import pytest
-import argparse
-
-def calculate_sum(a, b):
-    return a + b
-
 def test_calculate_sum():
     assert calculate_sum(2, 3) == 5
 
-@pytest.mark.parametrize("a, b, expected_sum", [
-    (1, 2, 3),
-    (0, 0, 0),
-    (-1, 2, 1),
-])
-def test_calculate_sum_parametrized(a, b, expected_sum):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", type=int, default=a)
-    parser.add_argument("-b", type=int, default=b)
-    args = parser.parse_args()
-    assert calculate_sum(args.a, args.b) == expected_sum
+# import pytest
+# import argparse
+
+# def calculate_sum(a, b):
+#     return a + b
+
+# def test_calculate_sum():
+#     assert calculate_sum(2, 3) == 5
+
+# @pytest.mark.parametrize("a, b, expected_sum", [
+#     (1, 2, 3),
+#     (0, 0, 0),
+#     (-1, 2, 1),
+# ])
+# def test_calculate_sum_parametrized(a, b, expected_sum):
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-a", type=int, default=a)
+#     parser.add_argument("-b", type=int, default=b)
+#     args = parser.parse_args()
+#     assert calculate_sum(args.a, args.b) == expected_sum
 
