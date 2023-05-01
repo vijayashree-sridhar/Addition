22bb864fffcd9946c0df4a48d79fc17ec3408348
vijayashree-sridhar <113469666+vijayashree-sridhar@users.noreply.github.com>
2023-05-02 00:59:05 +0530

    Update test_calculate_sum.py

--- test_calculate_sum.py
+++ test_calculate_sum.py
@@ -1,7 +1,7 @@
-from calculate_sum import calculate_sum
+from calculate import calculate
 
 def test_calculate_sum():
-    assert calculate_sum(2, 3) == 5
-    assert calculate_sum(2, 2) == 4
-    assert calculate_sum(2, 1) == 4
-    assert calculate_sum(2, 1) == 2
+    assert calculate_sum(2, 3, 1) == 5
+    assert calculate_sum(2, 2, 1) == 4
+    assert calculate_sum(20, 1, 1) == 21
+    assert calculate_sum(2, 10, 1) == 12
