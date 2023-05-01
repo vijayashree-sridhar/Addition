a925c9ba9ddc06d87a63f7758489c5cc4663c1da
vijayashree-sridhar <113469666+vijayashree-sridhar@users.noreply.github.com>
2023-05-02 00:56:26 +0530

    Update and rename calculate_sum.py to calculate.py

---
+++ calculate.py
@@ -0,0 +1,13 @@
+def calculate(a, b, c):
+    if(c==1):
+        return a + b
+    elif(c==2):
+        return a - b
+    elif(c==3):
+        return a * b
+    elif(c==4):
+        return a / b
+    elif(c==5):
+        return a % b
+    elif(c==6):
+        return a ** b
